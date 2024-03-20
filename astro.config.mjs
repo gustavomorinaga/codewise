8b9609d623dd648a5573677e021baed78f48e0f0
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-03-20 18:45:03 -0300

    feat: path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+	integrations: [tailwind(), svelte()]
 });
