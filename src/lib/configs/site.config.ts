00cf19765f7a45417d676b3c936b14fb6e09e6b6
Gustavo Morinaga <me@gustavomorinaga.dev>
2024-03-21 04:24:26 -0300

    feat: better folder structure

---
+++ src/lib/configs/site.config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+	name: 'CodeWise',
+	description: 'The best place to learn code',
+	url: 'https://codewise-academy.vercel.app',
+	author: 'gustavomorinaga',
+	links: {
+		twitter: 'https://twitter.com/gustavomorinaga',
+		github: 'https://github.com/gustavomorinaga/copyscotch'
+	},
+	keywords:
+		'code,programming,development,developer,software,web,app,html,css,react,angular,svelte,javascript,typescript'
+};
+
+export type SiteConfig = typeof siteConfig;
